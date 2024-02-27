4f36d346511d40ad30efcd38f06024bb54abb8c3
ChaLaam <124075393+DevChalaam@users.noreply.github.com>
2024-02-27 18:05:27 +0700

    "FIRST COMMIT"

---
+++ server.js
@@ -0,0 +1,17 @@
+// LOAD MODULES USE WITH SERVER
+const http = require('http');
+
+// SETTING PORT SERVER
+const port = 3000;
+
+// CERATE MY SERVER WITH NODE.JS
+const app = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.end("HELLO CHALAAM SERVER IS RUNNING!");
+    return
+});
+
+// LISTEN PORT SERVER
+app.listen(port, function() {
+    console.log("SERVER IS RUNNING ON PORT : 3000");
+});
